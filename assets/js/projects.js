554c88820abe22bab9a743f6ac4b83e2aafc8847
Jorenne Flores <jjflores@ucsd.edu>
2017-12-08 15:48:49 -0800

    redesign of projects to grid

--- assets/js/projects.js
+++ assets/js/projects.js
@@ -2,29 +2,40 @@
 function showAll() {
 	unboldAll();
 	$( "#all" ).addClass( "current" );
-	$( ".all" ).fadeOut( "fast", function() {});
-	$( ".all" ).fadeIn( "fast", function() {});
+	$( ".all" ).css("transition", ".5s ease");
+	$( ".all" ).css("opacity", "1");
+	//$( ".all" ).fadeOut( "fast", function() {});
+	//$( ".all" ).fadeIn( "fast", function() {});
 }
 
 function showArt() {
 	unboldAll();
 	$( "#art" ).addClass( "current" );
-	$( ".all" ).fadeOut( "fast", function() {});
-	$( ".art" ).fadeIn( "fast", function() {});	
+	$( ".all" ).css("transition", ".5s ease");
+	$( ".all" ).css("opacity", "0.2");
+	$( ".art" ).css("opacity", "1");
+	//$( ".all" ).fadeOut( "fast", function() {});
+	//$( ".art" ).fadeIn( "fast", function() {});	
 }
 
 function showDesign() {
 	unboldAll();
 	$( "#design" ).addClass( "current" );
-	$( ".all" ).fadeOut( "fast", function() {});
-	$( ".design" ).fadeIn( "fast", function() {});	
+	$( ".all" ).css("transition", ".5s ease");
+	$( ".all" ).css("opacity", "0.2");
+	$( ".design" ).css("opacity", "1");
+	//$( ".all" ).fadeOut( "fast", function() {});
+	//$( ".design" ).fadeIn( "fast", function() {});	
 }
 
 function showTechnical() {
 	unboldAll();
 	$( "#technical" ).addClass( "current" );
-	$( ".all" ).fadeOut( "fast", function() {});
-	$( ".technical" ).fadeIn( "fast", function() {});	
+	$( ".all" ).css("transition", ".5s ease");
+	$( ".all" ).css("opacity", "0.2");
+	$( ".technical" ).css("opacity", "1");
+	//$( ".all" ).fadeOut( "fast", function() {});
+	//$( ".technical" ).fadeIn( "fast", function() {});	
 }
 
 function unboldAll() {
