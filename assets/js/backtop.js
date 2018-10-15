ce532a04da742819f646cfd388c5e62686ce8837
Jorenne Flores <jjflores@ucsd.edu>
2018-10-14 18:33:10 -0700

    back to top animated

--- assets/js/backtop.js
+++ assets/js/backtop.js
@@ -1,17 +1,21 @@
 // BACK TO TOP
-window.onscroll = function() { scrollFunction() };
 
 // When the user scrolls down 200px from the top of the document, show the button
-function scrollFunction() {
-    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
-        document.getElementById("backtop").style.display = "block";
+$(window).scroll(function() {
+    var height = $(window).scrollTop();
+    if (height > 200) {
+        $('#backtop').fadeIn();
     } else {
-        document.getElementById("backtop").style.display = "none";
+        $('#backtop').fadeOut();
     }
-}
+});
 
 // When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-    document.body.scrollTop = 0; // For Safari
-    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
+$(document).ready(function() {
+    $("#backtop").click(function(event) {
+        event.preventDefault();
+        $("html, body").animate({ scrollTop: 0 }, "slow");
+        return false;
+    });
+
+});
