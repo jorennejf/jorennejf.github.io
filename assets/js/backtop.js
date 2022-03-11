6b1e81b6825a3b119b57e1b969bcebe6b6b594c0
Jorenne Flores <jorennejf@gmail.com>
2022-03-11 14:38:40 -0800

    fade in animation on load

--- assets/js/backtop.js
+++ assets/js/backtop.js
@@ -1,6 +1,4 @@
-// BACK TO TOP
-
-// When the user scrolls down 200px from the top of the document, show the button
+//BACK TO TOP - When the user scrolls down 200px from the top of the document, show the button
 $(window).scroll(function() {
     var height = $(window).scrollTop();
     if (height > 200) {
@@ -10,8 +8,10 @@ $(window).scroll(function() {
     }
 });
 
-// When the user clicks on the button, scroll to the top of the document
 $(document).ready(function() {
+    $(document.body).css('opacity', 1); //Fade in animation on load
+
+    //BACK TO TOP - When the user clicks on the button, scroll to the top of the document
     $("#backtop").click(function(event) {
         event.preventDefault();
         $("html, body").animate({ scrollTop: 0 }, "slow");
